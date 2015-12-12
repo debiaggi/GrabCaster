86ea94950dc9220348046e1c0f91984ad55dece4
ninocrudele <nino.crudele@live.com>
2015-12-12 10:35:20 +0000

    Implemented embedded feature
    The embedded feature is on with the high and deep level component

--- Framework.Contracts/Configuration/TriggerConfiguation.cs
+++ Framework.Contracts/Configuration/TriggerConfiguation.cs
@@ -6,16 +6,22 @@
 //    Author: Nino Crudele
 //    Blog:   http://ninocrudele.me
 //    Email:  nino.crudele@live.com
-//    Info:   http://GrabCaster.io
-// 
-//    By accessing GrabCaster code here, you are agreeing to the following licensing terms.
-//    If you do not agree to these terms, do not access the GrabCaster code.
-//    Your license to the GrabCaster source and/or binaries is governed by the 
+//    
 //    Reciprocal Public License 1.5 (RPL1.5) license as described here: 
 //    http://www.opensource.org/licenses/rpl1.5.txt
 //    
-//    This work is registered with the UK Copyright Service.
-//    Registration No:284695248  
+//    Unless explicitly acquired and licensed from Licensor under another
+//    license, the contents of this file are subject to the Reciprocal Public
+//    License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+//    and You may not copy or use this file in either source code or executable
+//    form, except in compliance with the terms and conditions of the RPL.
+//    
+//    All software distributed under the RPL is provided strictly on an "AS
+//    IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+//    LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+//    LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+//    PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+//    language governing rights and limitations under the RPL. 
 //  </summary>
 // --------------------------------------------------------------------------------------------------
 namespace GrabCaster.Framework.Contracts.Configuration
--- Laboratory.Console/Program.cs
+++ Laboratory.Console/Program.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file = "Program.cs" company="Nino Crudele">
+//   Copyright (c) 2015 Nino Crudele. All Rights Reserved.
+// </copyright>
+// <summary>
+//    Author: Nino Crudele
+//    Blog:   http://ninocrudele.me
+//    Email:  nino.crudele@live.com
+//    
+//    Reciprocal Public License 1.5 (RPL1.5) license as described here: 
+//    http://www.opensource.org/licenses/rpl1.5.txt
+//    
+//    Unless explicitly acquired and licensed from Licensor under another
+//    license, the contents of this file are subject to the Reciprocal Public
+//    License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+//    and You may not copy or use this file in either source code or executable
+//    form, except in compliance with the terms and conditions of the RPL.
+//    
+//    All software distributed under the RPL is provided strictly on an "AS
+//    IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+//    LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+//    LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+//    PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+//    language governing rights and limitations under the RPL. 
+//  </summary>
+// --------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
